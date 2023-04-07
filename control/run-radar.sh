6cfa7d9574ae618235ddd6dccf66e8962f5f9da1
Michael Christoffersen <mchristo28@gmail.com>
2023-04-07 15:07:29 -0800

    Added some documentation to run-radar

--- control/run-radar.sh
+++ control/run-radar.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Options:
+# stack - how many pulses to stack into each recorded trace
+# trigger - trigger threshold, comparison is absolute value so a threshold
+#           of X triggers on +/-X
+# spt - samples per trace (the radar has a fixed sampling rate of 20 MHz)
+# pretrig - number of pre-trigger samples to save in each trace
+
 filename=$(/home/radar/groundhog/control/generate-filename.py)
 
 gpspipe -d -r -t -o "$filename.txt"
-./src/radar --file "$filename.dat" --stack 500 --trigger 2500
+./src/radar --file "$filename.dat" --pretrig 8 --spt 512 --stack 500 --trigger 2500
 pkill gpspipe
